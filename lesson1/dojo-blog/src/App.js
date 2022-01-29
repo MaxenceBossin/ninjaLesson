b2cc2a51b45768b3f5ce21e0ffd4788417a6e7f0
MaxenceBossin <maxbossin25@gmail.com>
2022-01-29 11:00:44 +0100

    Premier template  react

--- lesson1/dojo-blog/src/App.js
+++ lesson1/dojo-blog/src/App.js
@@ -2,10 +2,23 @@
 import './App.css';
 
 function App() {
+  const title  = 'title'
+  const like   = 50
+  const person = { name : 'maxou'}
+  const link   = "http://google.com"
   return (
     <div className="App">
       <div className="content">
-        <h1>My frist component</h1>
+        <h1>{title}</h1>
+        <p>Like : {like} </p>
+        {/* test */}
+        <p>{10}</p>
+        <p>{'string'}</p>
+        <p>[1,array,2]</p>
+        <p>{Math.random() * 10}</p>
+        <p>{person.name}</p>
+        <a href={link}>google</a>
+        
       </div>
     </div>
   );
